7bc8792c81796bb022d1367b945714d203adfee7
saivarun <saivarunaitha1122@gmail.com>
2023-01-22 10:57:48 +0530

    first commit

---
+++ Lengthofword.js
@@ -0,0 +1,9 @@
+var lengthOfLastWord = function(s) {
+    const wordArray = s.trim().split(/(\s+)/)
+    const last = wordArray.length - 1
+    if (wordArray[last] === ''){
+       return 0 
+     }else {
+     return wordArray[last].length
+     }
+ }
